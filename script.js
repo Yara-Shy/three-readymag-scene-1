589053e47f71e2869dda43481ae7fe6099405dce
Yara-Shy <yara.shylyk.s@gmail.com>
2026-02-09 10:11:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
-// Licence CC BY-NC-SA 4.0
-// Attribution — You must give appropriate credit.
-// Non Commercial — You may not use the material for commercial purposes.
 
 import TubesCursor from "https://cdn.jsdelivr.net/npm/threejs-components@0.0.19/build/cursors/tubes1.min.js"
 
 const app = TubesCursor(document.getElementById('canvas'), {
   tubes: {
-    colors: ["#f967fb", "#53bc28", "#6958d5"],
+    colors: ["#f11b1b", "#139f40", "870b54", "#000000"],
     lights: {
-      intensity: 200,
-      colors: ["#83f36e", "#fe8a2e", "#ff008a", "#60aed5"]
+      intensity: 100,
+      colors: ["#f11b1b", "#139f40", "#870b54", "#2b4f11", "#000000"]
     }
   }
 })
 
 document.body.addEventListener('click', () => {
-  const colors = randomColors(3)
-  const lightsColors = randomColors(4)
+  const colors = randomColors(4)
+  const lightsColors = randomColors(5)
   console.log(colors, lightsColors)
   app.tubes.setColors(colors)
   app.tubes.setLightsColors(lightsColors)
